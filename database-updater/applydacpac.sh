b409cc9faf1d26f1b0b3825724e49d5889a539d5
LaxmanPanguluri <68398950+LaxmanPanguluri@users.noreply.github.com>
2020-11-24 12:09:09 -0500

    Update applydacpac.sh

--- database-updater/applydacpac.sh
+++ database-updater/applydacpac.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 
-# Not installed in this docker image
-# sqlcmd -S "${SQL_SERVER_URL},${SQL_SERVER_PORT}" \
-# -d "${SERVICE_DATABASE_NAME}" \
-# -U "${SQL_SERVER_USERNAME}" \
-# -P "${SQL_SERVER_PASSWORD}"
+set -e
 
-if [ $? -eq 0 ];
-then
-      echo "[INFO] Successfully logged in with credentials"
-elif [ $? -eq 401 ];
-then
-echo "[ERR] Unauthorized .Please check the credentials and try again"
-exit 1
-else
-   echo "[INFO] Failed to publish dacpac to sql database,Check the logs to debug the issue"
-exit 1
-fi 
-#Applying Dacpac present to database
-echo "Running SQL Package Command"
-sqlpackage \
-   /Action:Publish \
-   /SourceFile:"/dacpac/iUS.${SERVICE_NAME}.Database.SQL.dacpac" \ 
-   /TargetServerName:"${SQL_SERVER_URL},${SQL_SERVER_PORT}" \
-   /TargetDatabaseName:"${SERVICE_DATABASE_NAME}" \
-   /TargetUser:"${SQL_SERVER_USERNAME}" \
-   /TargetPassword:"${SQL_SERVER_PASSWORD}" \
-   /p:AllowIncompatiblePlatform=true \
-   /p:BlockOnPossibleDataLoss=false \
-   /TargetTimeout:120
+checkstatus(){
 
-if [ $? -eq 0 ];
-   echo "[INFO] Successfully Published dacpac to database"
+if [ $1 -eq 0 ];then
+   echo "[INFO] Successfully Ran the command"
 else
-   echo "[ERR] Failed to publish dacpac to sql database,Check the logs to debug the issue"
+   echo "[ERR] Failed to Run Command ,Check the logs to debug the issue"
    exit 1
 fi
+}
+# Doing az devops login to download the artifcat from feed
+#Fetching DB name to pass to sql package command.
+
+SERVICE_DATABASE_NAME=`python fetchdbname.py --appsettingsFilePath "/appsettings.idaho.development.json" --CannonicalApplicationName ${SERVICE_NAME}`
+checkstatus $?
+
+echo ${PAT_TOKEN} | az devops login
+checkstatus $?
+
+az artifacts universal download --organization ${AZURE_URL} --feed ${FEED} --name ${PROJECT} --version ${VERSION} --path /dacpac
+checkstatus $?
+
+#Applying Dacpac present to database
+echo "Running SQL Package Command"
+
+cd /dacpac/DacPac/
+dacpacFilename=`ls | grep -i ".dacpac"`
+
+echo $dacpacFilename
+/opt/sqlpackage/sqlpackage /Action:Publish /SourceFile:"/dacpac/DacPac/${dacpacFilename}" /TargetServerName:"${SQL_SERVER_URL},${SQL_SERVER_PORT}" /TargetDatabaseName:"${SERVICE_DATABASE_NAME}" /TargetUser:"${SQL_SERVER_USERNAME}" /TargetPassword:"${SQL_SERVER_PASSWORD}" /p:AllowIncompatiblePlatform=true /p:BlockOnPossibleDataLoss=false /TargetTimeout:120
+checkstatus $?
