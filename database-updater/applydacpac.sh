9dec3ca32fb30d819e486bc74d16df3dbf01dbd3
LaxmanPanguluri <68398950+LaxmanPanguluri@users.noreply.github.com>
2020-12-02 00:16:27 -0500

    Update applydacpac.sh

--- database-updater/applydacpac.sh
+++ database-updater/applydacpac.sh
@@ -14,21 +14,22 @@ fi
 # Doing az devops login to download the artifcat from feed
 #Fetching DB name to pass to sql package command.
 
-SERVICE_DATABASE_NAME=`python fetchdbname.py --appsettingsFilePath "/app/appsettings/appsettings.idaho.development.json" --CannonicalApplicationName ${SERVICE_NAME}`
+SERVICE_DATABASE_NAME=python fetchdbname.py --appsettingsFilePath "/app/appsettings/appsettings.idaho.development.json" --CannonicalApplicationName ${SERVICE_NAME}
 checkstatus $?
 
 echo ${PAT_TOKEN} | az devops login
 checkstatus $?
 
-az artifacts universal download --organization ${AZURE_URL} --feed ${FEED} --name ${PROJECT} --version ${VERSION} --path /dacpac
+ARTIFACT_VERSION=echo $VERSION | cut -d "." -f2-
+az artifacts universal download --organization ${AZURE_URL} --feed ${FEED} --name ${PROJECT}.dacpac-artifact --version ${ARTIFACT_VERSION} --path /dacpac
 checkstatus $?
 
 #Applying Dacpac present to database
 echo "Running SQL Package Command"
 
 cd /dacpac/DacPac/
-dacpacFilename=`ls | grep -i ".dacpac"`
+dacpacFilename=ls | grep -i ".dacpac"
 
 echo $dacpacFilename
-/opt/sqlpackage/sqlpackage /Action:Publish /SourceFile:"/dacpac/DacPac/${dacpacFilename}" /TargetServerName:"${SQL_SERVER_URL},${SQL_SERVER_PORT}" /TargetDatabaseName:"${SERVICE_DATABASE_NAME}" /TargetUser:"${SQL_SERVER_USERNAME}" /TargetPassword:"${SQL_SERVER_PASSWORD}" /p:AllowIncompatiblePlatform=true /p:BlockOnPossibleDataLoss=false /TargetTimeout:120
+/opt/sqlpackage/sqlpackage /Action:Publish /SourceFile:"/dacpac/DacPac/${dacpacFilename}" /TargetServerName:"${SQL_SERVER_URL},${SQL_SERVER_PORT}" /TargetDatabaseName:"${SERVICE_DATABASE_NAME}" /TargetUser:"${SQL_USERNAME}" /TargetPassword:"${SQL_PASSWORD}" /p:AllowIncompatiblePlatform=true /p:BlockOnPossibleDataLoss=false /TargetTimeout:120
 checkstatus $?
